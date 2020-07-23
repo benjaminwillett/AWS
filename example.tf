ab2e7803dc2dff966bf86a3b81cbe58a9d1ab9c4
Ben Willett <ben.willett@distortenterprises.com>
2020-07-24 08:49:03 +1000

    Update example.tf

--- example.tf
+++ example.tf
@@ -11,7 +11,6 @@ provisioner "local-exec" {
     command = "echo ${aws_instance.server1.public_ip} > ip_address.txt"
 }
 }
-
 resource "aws_instance" "server2" {
   ami           = "ami-020d764f9372da231"
   instance_type = "t2.micro"
@@ -21,11 +20,15 @@ provisioner "local-exec" {
 }
   }
 resource "aws_instance" "foo" {
+  ami           = "ami-0e729696b8705907b"
+  instance_type = "t2.micro"
 }
 output "server1_ip" {
   value = aws_instance.server1.public_ip
 }
-
 output "server2_ip" {
   value = aws_instance.server2.public_ip
+}
+output "foo_ip" {
+  value = aws_instance.foo.public_ip
 }
